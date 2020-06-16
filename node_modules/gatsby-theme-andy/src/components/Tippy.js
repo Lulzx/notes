f5e948cba96688eaac2edafbf4287879d24beb80
Lulzx <wiserishabh@gmail.com>
2020-06-16 07:51:24 +0000

    init

--- node_modules/gatsby-theme-andy/src/components/Tippy.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import Tippy from '@tippyjs/react';
-import 'tippy.js/animations/shift-away.css';
-
-export default (props) => <Tippy placement="right" animation="slide-away" {...props} />;
